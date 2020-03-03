57c4ab6a69cd2c21406a5de9dc0ea8f55d67354b
Toby Bessant <toby.bessant.1998@gmail.com>
2020-03-03 23:09:53 +0000

    Implemented permittedAccountTypes middleware to pass tests

---
+++ server/src/services/middleware/permittedAccountTypes.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { UNAUTHORIZED } from "http-status-codes";
+
+export class PermittedAccountTypes {
+  public static are(types: string[]) {
+    return function (req: Request, res: Response, next: NextFunction) {
+      const type = (req.user as any).type;
+
+      if (!types.includes(type)) {
+        res.status(UNAUTHORIZED);
+        return;
+      }
+
+      next();
+    }
+  }
+}
