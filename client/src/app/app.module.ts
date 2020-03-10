dd723af1fcad0b8668e2f4c9e6f246eaf15bd2b2
Toby Bessant <toby.bessant.1998@gmail.com>
2020-03-10 21:42:41 +0000

    Fixed single quote import

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { initApp } from "./app-initialiser";
 import { SessionService } from "./services/session.service";
 import { NavComponent } from "./components/common/nav/nav.component";
 import { ProjectComponent } from "./components/supplier/project/project.component";
-import { ConfirmationPromptComponent } from './components/common/confirmation-prompt/confirmation-prompt.component';
+import { ConfirmationPromptComponent } from "./components/common/confirmation-prompt/confirmation-prompt.component";
 
 @NgModule({
   declarations: [
