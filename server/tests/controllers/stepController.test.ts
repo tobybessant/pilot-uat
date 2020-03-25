46c6a80e8a9bb04c739bb31096a9a41c54cf85ea
Toby Bessant <toby.bessant.1998@gmail.com>
2020-03-25 15:33:08 +0000

    Added remaining tests for stepController

--- server/tests/controllers/stepController.test.ts
+++ server/tests/controllers/stepController.test.ts
@@ -8,9 +8,10 @@ import { StepDbo } from "../../src/database/entities/stepDbo";
 import { IStepResponse } from "../../src/dto/response/supplier/step";
 import { StepStatus, StepStatusDbo } from "../../src/database/entities/stepStatusDbo";
 import { CaseRepository } from "../../src/repositories/caseRepository";
-import { OK, INTERNAL_SERVER_ERROR } from "http-status-codes";
+import { OK, INTERNAL_SERVER_ERROR, BAD_REQUEST } from "http-status-codes";
 import { BaseController } from "../../src/controllers/baseController";
 import { IGetAllStepsRequest } from "../../src/dto/request/supplier/getAllSteps";
+import { IUpdateStepRequest } from "../../src/dto/request/supplier/updateStep";
 
 suite("Step Controller", () => {
   let stepRepository: IMock<StepRepository>;
@@ -167,12 +168,199 @@ suite("Step Controller", () => {
 
         res.verify(r => r.status(OK), Times.once());
       });
+    });
+
+    suite("Unexpected 'Error' thrown by stepRepository", () => {
+
+      setup(() => {
+        getStepsBody = {
+          caseId: "9"
+        };
+      });
+
+      test(`Generic error ${BaseController.INTERNAL_SERVER_ERROR_MESSAGE} is returned in response errors array`, async () => {
+        given_Request_body_is(getStepsBody);
+        given_stepRepository_getStepsForCase_throws();
+
+        await subject.getStepsForCase(req.object, res.object);
+
+        res.verify(r => r.json({ errors: [BaseController.INTERNAL_SERVER_ERROR_MESSAGE] }), Times.once());
+      });
+
+      test("Response returns statusCode 500", async () => {
+        given_Request_body_is(getStepsBody);
+        given_stepRepository_getStepsForCase_throws();
+
+        await subject.getStepsForCase(req.object, res.object);
+
+        res.verify(r => r.status(INTERNAL_SERVER_ERROR), Times.once());
+      });
+    });
+  });
+
+  suite("Update Step", () => {
+    let updateStepBody: IUpdateStepRequest;
+
+    let stepStatus: StepStatusDbo;
+    let originalStep: StepDbo;
+    let updatedStep: StepDbo;
+    let updatedStepResponse: IStepResponse;
+
+    suite("Valid request conditions", () => {
+
+      setup(() => {
+        updateStepBody = {
+          id: "4",
+          description: "New Description"
+        };
+
+        stepStatus = new StepStatusDbo();
+        stepStatus.id = 90;
+        stepStatus.label = StepStatus.PASSED;
+
+        originalStep = new StepDbo();
+        originalStep.description = "Original description...";
+        originalStep.id = Number(updateStepBody.id);
+        originalStep.status = stepStatus;
+
+        updatedStep = new StepDbo();
+        updatedStep.description = updateStepBody.description!;
+        updatedStep.id = Number(updateStepBody.id);
+        updatedStep.status = stepStatus;
+
+        updatedStepResponse = {
+          description: updatedStep.description,
+          id: updatedStep.id.toString(),
+          status: {
+            id: updatedStep.status.id.toString(),
+            label: updatedStep.status.label
+          }
+        }
+      });
+
+      test("Returns updated case in response payload", async () => {
+        test("Saved step is returned in response body", async () => {
+          given_Request_body_is(updateStepBody);
+          given_stepRepository_getStepById_returns(originalStep);
+          given_stepRepository_updateStep_returns(updatedStep);
+
+          await subject.addStepToCase(req.object, res.object);
+
+          res.verify(r => r.json({ errors: [], payload: updatedStepResponse }), Times.once());
+        });
+      });
+
+      test("Returns statusCode 200 in response", async () => {
+        test("Saved step is returned in response body", async () => {
+          given_Request_body_is(updateStepBody);
+          given_stepRepository_getStepById_returns(originalStep);
+          given_stepRepository_updateStep_returns(updatedStep);
+
+          await subject.addStepToCase(req.object, res.object);
+
+          res.verify(r => r.status(OK), Times.once());
+        });
+      });
+
+    });
+
+    suite("Suite could not be found", () => {
+
+      setup(() => {
+        updateStepBody = {
+          id: "4",
+          description: "New Description"
+        };
+      });
+
+      test("Error 'Error finding step' returned in errors array", async () => {
+        given_Request_body_is(updateStepBody);
+        given_stepRepository_getStepById_returns(undefined);
+
+        await subject.updateStep(req.object, res.object);
+
+        res.verify(r => r.json({ errors: ["Error finding step"] }), Times.once());
+      });
+
+      test("Response returns statusCode 400", async () => {
+        given_Request_body_is(updateStepBody);
+        given_stepRepository_getStepById_returns(undefined);
+
+        await subject.updateStep(req.object, res.object);
+
+        res.verify(r => r.status(BAD_REQUEST), Times.once());
+      });
 
     });
 
 
-  });
+    suite("Unexpected 'Error' thrown by stepRepository getStepById", () => {
 
+      setup(() => {
+        updateStepBody = {
+          id: "4",
+          description: "New Description"
+        };
+      });
+
+      test(`Generic error ${BaseController.INTERNAL_SERVER_ERROR_MESSAGE} returned in errors array`, async () => {
+        given_Request_body_is(updateStepBody);
+        given_stepRepository_getStepById_throws();
+
+        await subject.updateStep(req.object, res.object);
+
+        res.verify(r => r.json({ errors: [BaseController.INTERNAL_SERVER_ERROR_MESSAGE] }), Times.once());
+      });
+
+      test("Response returns statusCode 500", async () => {
+        given_Request_body_is(updateStepBody);
+        given_stepRepository_getStepById_throws();
+
+        await subject.updateStep(req.object, res.object);
+
+        res.verify(r => r.status(INTERNAL_SERVER_ERROR), Times.once());
+      });
+    });
+
+    suite("Unexpected 'Error' thrown by stepRepository updateStep", () => {
+
+      setup(() => {
+        updateStepBody = {
+          id: "4",
+          description: "New Description"
+        };
+
+        stepStatus = new StepStatusDbo();
+        stepStatus.id = 90;
+        stepStatus.label = StepStatus.PASSED;
+
+        originalStep = new StepDbo();
+        originalStep.description = "Original description...";
+        originalStep.id = Number(updateStepBody.id);
+        originalStep.status = stepStatus;
+      });
+
+      test(`Generic error ${BaseController.INTERNAL_SERVER_ERROR_MESSAGE} returned in errors array`, async () => {
+        given_Request_body_is(updateStepBody);
+        given_stepRepository_getStepById_returns(originalStep);
+        given_stepRepository_updateStep_throws();
+
+        await subject.updateStep(req.object, res.object);
+
+        res.verify(r => r.json({ errors: [BaseController.INTERNAL_SERVER_ERROR_MESSAGE] }), Times.once());
+      });
+
+      test("Response returns statusCode 500", async () => {
+        given_Request_body_is(updateStepBody);
+        given_stepRepository_getStepById_returns(originalStep);
+        given_stepRepository_updateStep_throws();
+
+        await subject.updateStep(req.object, res.object);
+
+        res.verify(r => r.status(INTERNAL_SERVER_ERROR), Times.once());
+      });
+    });
+  });
 
   function given_Request_body_is(body: any) {
     req
@@ -195,5 +383,34 @@ suite("Step Controller", () => {
     stepRepository
       .setup(sr => sr.getStepsForCase(It.isAny()))
       .returns(async () => steps);
+  }
+
+  function given_stepRepository_getStepsForCase_throws() {
+    stepRepository
+      .setup(sr => sr.getStepsForCase(It.isAny()))
+      .throws(new Error("Sensitive database information!"));
+  }
+
+  function given_stepRepository_getStepById_returns(step: StepDbo | undefined) {
+    stepRepository
+      .setup(sr => sr.getStepById(It.isAny()))
+      .returns(async () => step);
+  }
+
+  function given_stepRepository_updateStep_returns(step: StepDbo) {
+    stepRepository
+      .setup(sr => sr.updateStep(It.isAny()))
+      .returns(async () => step);
+  }
+  function given_stepRepository_getStepById_throws() {
+    stepRepository
+      .setup(sr => sr.getStepById(It.isAny()))
+      .throws(new Error("Sensetive database information!"));
+  }
+
+  function given_stepRepository_updateStep_throws() {
+    stepRepository
+      .setup(sr => sr.updateStep(It.isAny()))
+      .throws(new Error("Sensetive database information!"));
   }
 });
