317741b01fe28e4bdbea99d770745760c2bea485
Toby Bessant <toby.bessant.1998@gmail.com>
2020-03-30 18:45:34 +0100

    Added 48hr expiry to invite JWT

--- server/src/services/utils/jwtService.ts
+++ server/src/services/utils/jwtService.ts
@@ -1,17 +1,27 @@
 import { injectable } from "tsyringe";
 import * as jwt from "jwt-simple";
 import { IProjectInviteToken } from "../../dto/request/common/inviteToken";
+import { ApiError } from "../apiError";
+import { UNAUTHORIZED } from "http-status-codes";
 
 @injectable()
 export class JwtService {
 
   private readonly secret: string = process.env.JWT_SECRET || "shhh";
+  private readonly EXPIRY_HOURS = 48;
 
   public encode(inviteData: IProjectInviteToken): string {
-    return jwt.encode(inviteData, this.secret);
+    return jwt.encode({
+      ...inviteData,
+      exp: Date.now() + (this.EXPIRY_HOURS * 60 * 1000)
+    }, this.secret);
   }
 
   public decode(token: string): IProjectInviteToken {
-    return jwt.decode(token, this.secret);
+    try {
+      return jwt.decode(token, this.secret);
+    } catch(error) {
+      throw new ApiError(error.message, UNAUTHORIZED, true);
+    }
   }
 }
