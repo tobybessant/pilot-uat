d1fb2567379e384dc04fa65bd0404b8b7e86d279
Toby Bessant <toby.bessant.1998@gmail.com>
2020-05-07 19:02:41 +0100

    Removed password protocols from server

--- server/src/services/middleware/joi/schemas/createUser.ts
+++ server/src/services/middleware/joi/schemas/createUser.ts
@@ -29,9 +29,6 @@ export class CreateUser implements ICreateUserRequest {
     .label("Password")
     .min(8)
     .max(64)
-    .regex(/[a-z]/)
-    .regex(/[A-Z]/)
-    .regex(/[0-9]/)
     .required()
   password!: string;
 
