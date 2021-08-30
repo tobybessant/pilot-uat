6cf306134b526e7b4015685d26a6a0ed070b4bab
Toby Bessant <toby.bessant.1998@gmail.com>
2021-08-30 15:59:27 +0100

    Persist DB to file

--- server/src/database/seeds/seedConstants.ts
+++ server/src/database/seeds/seedConstants.ts
@@ -1,10 +1,5 @@
-import { Bcrypt } from "../../services/utils/bcryptHash";
 import { DatabaseSeeder } from "./databaseSeeder";
-import { TABLE_NAME as PRJOECT_TABLE_NAME } from "../entities/projectDbo";
-import { TABLE_NAME as USER_TABLE_NAME } from "../entities/userDbo";
 import { TABLE_NAME as USERTYPE_TABLE_NAME } from "../entities/userTypeDbo";
-import { TABLE_NAME as ORGANISATION_TABLE_NAME } from "../entities/organisationDbo";
-import { TABLE_NAME as USER_PROJECT_ROLE_TABLE_NAME } from "../entities/userProjectRoleDbo";
 import {
   StepStatus,
   TABLE_NAME as STEPSTATUS_TABLE_NAME,
@@ -12,8 +7,6 @@ import {
 
 const seeder: DatabaseSeeder = new DatabaseSeeder();
 const databaseSeedData: Map<string, any[]> = new Map<string, any[]>();
-
-const pwHash = new Bcrypt().hash("password1");
 
 databaseSeedData.set(USERTYPE_TABLE_NAME, [
   { type: "Supplier" },
@@ -26,7 +19,7 @@ databaseSeedData.set(STEPSTATUS_TABLE_NAME, [
   { label: StepStatus.FAILED },
 ]);
 
-export async function seedDemoData() {
+export async function seedApplicationConstants() {
   try {
     await seeder.seed(databaseSeedData);
   } catch (e) {
